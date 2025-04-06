705986c7674fe2ed8a403fa19412554dcbe16615
jellygz <jilliangonzales12@gmail.com>
2025-04-06 00:28:47 -0700

    button to ProjectReflection

--- src/app/components/ProjectReflection/index.js
+++ src/app/components/ProjectReflection/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './ProjectReflection.module.css'
 
 export default function ProjectReflection({ content, url }) {
   return (
     <section className={styles.container}>
+      <div className={styles.contentWrapper}>
         <h3 className={styles.title}>Design Approach</h3>
         <p className={styles.content}>{content}</p>
         <div className={styles.imageWrapper}>
-          <Image className={styles.image} src={url} alt='mockup' width={200} height={200} />
+          <Image className={styles.image} src={url} alt="mockup" width={200} height={200} />
         </div>
+        <Link href="/" className={styles.homeButton}>← Go Back Home</Link>
+      </div>
     </section>
   )
 }
